030083a287a6b24c137d43cc3ab1f17f7b0891c3
Dawid <stodoo@gmail.com>
2019-01-06 16:06:37 +0100

    initial commit - with SampleController

---
+++ src/main/java/com/funjavablog/dockerizespringboot/DockerizeSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.funjavablog.dockerizespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeSpringBootApplication.class, args);
+	}
+
+}
+
